796495614fc3322e462eed8ca12867097b59d943
Mitesh <Mitesh@192.168.1.103>
2012-10-18 10:01:49 +0530

    Added pin image to particular location(Showed default our office address on map)

--- GoolgeMapTest/src/com/goolgemaptest/MainActivity.java
+++ GoolgeMapTest/src/com/goolgemaptest/MainActivity.java
@@ -1,11 +1,19 @@
 package com.goolgemaptest;
 
+import java.util.List;
+
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
 import android.support.v4.view.ViewPager.LayoutParams;
 import android.view.Menu;
 import android.view.View;
@@ -15,6 +23,26 @@ public class MainActivity extends MapActivity {
 
 	GeoPoint p;
 	MapController mc;
+	 
+	class MapOverlay extends com.google.android.maps.Overlay
+	    {
+	        @Override
+	        public boolean draw(Canvas canvas, MapView mapView, 
+	        boolean shadow, long when) 
+	        {
+	            super.draw(canvas, mapView, shadow);                   
+	 
+	            //---translate the GeoPoint to screen pixels---
+	            Point screenPts = new Point();
+	            mapView.getProjection().toPixels(p, screenPts);
+	 
+	            //---add the marker---
+	            Bitmap bmp = BitmapFactory.decodeResource(
+	                getResources(), R.drawable.pushpin);            
+	            canvas.drawBitmap(bmp, screenPts.x, screenPts.y-50, null);         
+	            return true;
+	        }
+	    } 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +57,7 @@ public class MainActivity extends MapActivity {
                 LayoutParams.WRAP_CONTENT)); 
         mapView.displayZoomControls(true);
         mc = mapView.getController();
-        String coordinates[] = {"1.352566007", "103.78921587"};
+        String coordinates[] = {"20.371237", "72.906340"};
         double lat = Double.parseDouble(coordinates[0]);
         double lng = Double.parseDouble(coordinates[1]);
  
@@ -39,6 +67,10 @@ public class MainActivity extends MapActivity {
  
         mc.animateTo(p);
         mc.setZoom(17); 
+        MapOverlay mapOverlay = new MapOverlay();
+        List<Overlay> listOfOverlays = mapView.getOverlays();
+        listOfOverlays.clear();
+        listOfOverlays.add(mapOverlay);       
         mapView.invalidate();
     }
 
